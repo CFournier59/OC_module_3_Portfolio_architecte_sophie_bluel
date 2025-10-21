79a7b5c6cf046240af71404c8dee06137fa9e2ad
CFournier59 <clementfournier.c@gmail.com>
2025-10-21 12:20:00 +0200

    integration dynamique des boutons de filtre: - sur index.html: ajout du bouton de filtrage par défaut "tous" -sur style.css: ajout des propriétés de styles conformes au rendu maquette - sur works.js: ajout des scripts necessaires à l'integration et au filtrage dynamique

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,11 +1,12 @@
 // definition des constantes
 const gallery = document.querySelector(".gallery")
+const buttonContainer = document.querySelector(".filter-button-container")
 
-
-// demande du contenu "works auprès de l'API"
-const reponse = await fetch("http://localhost:5678/api/works")
+// demande des contenus works et gategories auprès de l'API
+let reponse = await fetch("http://localhost:5678/api/works")
 const works = await reponse.json()
-
+reponse = await fetch("http://localhost:5678/api/categories")
+const categories = await reponse.json()
 
 //fonction d'affichage des travaux
 function displayWorks(works){
@@ -22,4 +23,25 @@ function displayWorks(works){
     }
 }
 
+//mise en place des boutons de filtres
+//le premier bouton est statique et affiche tous les travaux
+const allButton = document.getElementById("allWorks")
+allButton.addEventListener("click", () =>{
+    gallery.innerHTML = ""
+    displayWorks(works)
+})
+// les autres sont génerés selon les catégories définies sur l'API
+for(let i=0; i<categories.length; i++){
+    const filterButton = document.createElement("button")
+    filterButton.innerText = categories[i].name
+    buttonContainer.appendChild(filterButton)
+    filterButton.addEventListener("click", () => {
+        const filteredWorks = works.filter((work) => work.categoryId === i+1)
+        gallery.innerHTML = ""
+        displayWorks(filteredWorks)
+
+    })
+}
+
+//appel de la fonction d'affichage
 displayWorks(works)

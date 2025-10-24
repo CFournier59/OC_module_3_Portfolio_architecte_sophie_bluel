a27a5dcae0fc847850c2156bf417a84a61d89b9e
CFournier59 <clementfournier.c@gmail.com>
2025-10-24 11:03:28 +0200

    refactorisation du script de la page login: -  simplification du système de message d'erreur

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,48 +1,36 @@
-// définition des constantes 
-const loginForm = document.querySelector("form")
-
 //fonction d'affichage d'une erreur
-function displayErrorMessage(message){
-    let errorElement = document.getElementById("errorElement")
-    if(!errorElement){  
-        errorElement = document.createElement("p")
-        errorElement.id = "errorElement"
-        errorElement.style = ("color: red")  
-        loginForm.appendChild(errorElement)
-    }
-    errorElement.innerText = message
+function displayErrorMessage(){
+    let errorElement = document.querySelector(".error-message")
+    errorElement.classList.remove("d-none")
 }
-
-//écoute de l'évènement "submit"
-loginForm.addEventListener ("submit", async (event) => {
-    event.preventDefault()
-    // construction de la requête POST
-    const payload = {
-        email: event.target.querySelector("input[type=email]").value,
-        password: event.target.querySelector("input[type=password]").value.trim()
-    }
-    //envoi de la requête de connexion
-    try{
+// fonction principale de la page login
+async function mainLogin(){
+    //écoute de l'évènement "submit"
+    document.querySelector("form").addEventListener ("submit", async (event) => {
+        event.preventDefault()
+        // construction de la requête POST
+        const payload = {
+            email: event.target.querySelector("input[type=email]").value.trim(),
+            password: event.target.querySelector("input[type=password]").value.trim()
+        }
+        //envoi de la requête de connexion
         const reponse = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(payload)
         })
-        switch(reponse.status){
-            case 404: 
-                throw new Error("utilisateur non reconnu, vérifiez l'addresse email")
-                break
-            case 401:
-                throw new Error("accès refusé. mot de passe incorrect")
-                break
-            default:
-                const token = await reponse.json()
-                localStorage.setItem("currentToken", JSON.stringify(token))   
-                location.href = "index.html"
-        }
-    }catch(error){
-        displayErrorMessage(error.message)
-    }
-})
+        // si identifiants ok, capture du token et redirection
+        if(reponse.ok){
+            const token = await reponse.json()
+            localStorage.setItem("currentToken", JSON.stringify(token))  
+            location.href = "index.html" 
+        }  
+        else{
+            displayErrorMessage()
+        }    
+    })
+}
+//appel de la fonction principale de la page
+await mainLogin()
 
 

bf8fe3be65034b3c9bff2e5888fa0eef5c5799a9
CFournier59 <clementfournier.c@gmail.com>
2025-10-27 09:31:17 +0100

    retouches sur work.js : - modif de la boucle en "for of" sur la fonction disyplayWorks - suppression du console.log à la fin du script

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -2,13 +2,13 @@
 function displayWorks(works){
     const gallery = document.querySelector(".gallery")
     gallery.innerHTML = ""
-    for(let i=0; i<works.length; i++){
+    for(const work of works){
         const workFigure = document.createElement("figure")
         const workImage = document.createElement("img")
         const workFigCaption = document.createElement("figcaption")
-        workImage.src = works[i].imageUrl
-        workImage.alt = works[i].title
-        workFigCaption.innerText = works[i].title
+        workImage.src = work.imageUrl
+        workImage.alt = work.title
+        workFigCaption.innerText = work.title
         gallery.appendChild(workFigure)
         workFigure.appendChild(workImage)
         workFigure.appendChild(workFigCaption)
@@ -67,7 +67,6 @@ async function mainWorks() {
     }
     //appel de la fonction d'affichage
     displayWorks(works)
-    console.log("displayworks")
 }
 // pour finir, appel de la fonction principale
 await mainWorks()
